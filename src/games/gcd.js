0202be407855e18d2053724fe8e153e6e3df2ccb
donerin <donerin1404@gmail.com>
2022-09-16 16:16:14 +0300

    add logic

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,36 +1,25 @@
 /* eslint-disable no-console, import/extensions */
-import readlineSync from 'readline-sync';
 import random from '../randomNumber.js';
+import originBrain from '../index.js';
 
-const brainGcd = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Find the greatest common divisor of given numbers.');
-  const getGcd = (numberOne, numberTwo) => {
-    while (numberTwo) {
-      const numberSum = numberTwo;
-      numberTwo = numberOne % numberTwo;
-      numberOne = numberSum;
-    }
-    return numberOne;
-  };
-
-  for (let i = 0; i < 3; i += 1) {
-    const valueOne = random(1, 50);
-    const valueTwo = random(1, 50);
-    console.log(`Question: ${valueOne} ${valueTwo}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const trueAnswer = getGcd(valueOne, valueTwo);
-    const wrongAnswer = `${userAnswer} is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${userName}!`;
-
-    if (Number(userAnswer) === trueAnswer) {
-      console.log('Correct!');
-    } else {
-      return console.log(wrongAnswer);
-    }
+const description = 'Find the greatest common divisor of given numbers.';
+const getGcd = (numberOne, numberTwo) => {
+  while (numberTwo) {
+    const numberSum = numberTwo;
+    numberTwo = numberOne % numberTwo;
+    numberOne = numberSum;
   }
-  console.log(`Congratulations, ${userName}!`);
+  return numberOne;
 };
+
+const information = () => {
+  const valueOne = random(1, 50);
+  const valueTwo = random(1, 50);
+  const question = `${valueOne} ${valueTwo}`;
+  const trueAnswer = getGcd(valueOne, valueTwo);
+  return [question, trueAnswer];
+};
+
+const brainGcd = () => originBrain(description, information);
 
 export default brainGcd;
