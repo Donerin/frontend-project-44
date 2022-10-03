3865fb4a124229e2a0c89a28296eba40ac7b9540
donerin <donerin1404@gmail.com>
2022-10-03 13:11:47 +0300

    add changes

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import getRandomNumber from '../randomNumber.js';
 import getOriginBrain from '../index.js';
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
 const isEven = (num) => (num % 2) === 0;
 
