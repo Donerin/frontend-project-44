6d85d3caaa57cc712692c927a3e37892a518b035
donerin <donerin1404@gmail.com>
2022-10-03 12:43:24 +0300

    add one change

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-const getOriginBrain = (description, information) => {
+const getOriginBrain = (description, getInformation) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
   for (let i = 0; i < roundsCount; i += 1) {
-    const [question, rightAnswer] = information();
+    const [question, rightAnswer] = getInformation();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
